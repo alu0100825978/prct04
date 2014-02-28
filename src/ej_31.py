d92d1fed4ed60c2c511d40cdec89c7fd8114b289
alu0100825978@ull.edu.es <alu0100825978@ull.edu.es>
2014-02-28 10:27:02 +0000

    fichero 31

--- src/ej_31.py
+++ src/ej_31.py
@@ -3,5 +3,5 @@
 numero = int (raw_input('Introduzca un numero '))
 
 for potencia in [2,3,4,5]:
-  print '%d elevado a %d es 5d' % (numero, potencia, numero**potencia)
+  print '%d elevado a %d es %d' % (numero, potencia, numero**potencia)
 
